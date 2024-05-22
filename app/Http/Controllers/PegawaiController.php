53b8ceeb5524247f51cfeba9cea125654ea88854
azminadita <azminaditaputri24@gmail.com>
2024-05-22 14:42:58 +0700

    formpegawai(selesai)

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Pegawai;
 
 class PegawaiController extends Controller
 {
@@ -11,7 +12,9 @@ class PegawaiController extends Controller
      */
     public function index()
     {
-        //
+        $nomor = 1;
+        $peg = Pegawai::all(); //eloquent ORM
+        return view('pegawai.index',compact('nomor','peg'));
     }
 
     /**
